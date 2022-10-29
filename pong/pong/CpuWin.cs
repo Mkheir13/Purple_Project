5fc377b1829a7783fb289df529d7aab28e279527
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-10-29 13:13:36 +0200

    pong with collision, ball movements, and paddel movements

---
+++ pong/pong/CpuWin.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class CpuWin : Form
+    {
+        public CpuWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ pong/pong/PlayerWin.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class PlayerWin : Form
+    {
+        public PlayerWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
