1b56b7174512a5a85150cf454bd7dad679ae5d8b
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-10-30 02:47:27 +0100

    player collision but can't jump on a platform

--- Platform/Platform/Game.Designer.cs
+++ Platform/Platform/Game.Designer.cs
@@ -29,29 +29,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel = new System.Windows.Forms.Panel();
             this.player = new System.Windows.Forms.PictureBox();
             this.timer = new System.Timers.Timer();
-            this.panel.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.timer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel
-            // 
-            this.panel.Controls.Add(this.player);
-            this.panel.Location = new System.Drawing.Point(0, 1);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(802, 449);
-            this.panel.TabIndex = 0;
-            this.panel.Tag = "panel";
             // 
             // player
             // 
-            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.player.Location = new System.Drawing.Point(12, 399);
+            this.player.BackColor = System.Drawing.Color.Coral;
+            this.player.Location = new System.Drawing.Point(12, 400);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(30, 50);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.player.TabIndex = 0;
             this.player.TabStop = false;
             this.player.Tag = "player";
@@ -63,28 +55,42 @@
             this.timer.SynchronizingObject = this;
             this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(302, 354);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 33);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Platform.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.panel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            this.panel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.timer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.PictureBox pictureBox1;
+
         private System.Timers.Timer timer;
 
-        private System.Windows.Forms.Panel panel;
         private System.Windows.Forms.PictureBox player;
 
         #endregion
--- Platform/Platform/Game.cs
+++ Platform/Platform/Game.cs
@@ -7,7 +7,7 @@ namespace Platform
     public partial class Form1 : Form
     {
         bool _right, _left, _jump;
-        int _gravity = 10;
+        int _gravity = 15;
         int _force;
         public Form1()
         {
@@ -35,6 +35,36 @@ namespace Platform
 
         private void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
+            foreach (Control x in this.Controls)
+            { 
+                if (x is PictureBox && (string)x.Tag == "platform")
+                { 
+                    if (player.Bottom > x.Top && player.Bottom < x.Bottom && player.Left < x.Right && player.Right > x.Left)
+                    {
+                        _jump = false;
+                        _force = _gravity;
+                        player.Top = x.Top - player.Height;
+                    } 
+                    if (player.Top < x.Bottom && player.Top > x.Top && player.Left < x.Right && player.Right > x.Left)
+                    {
+                        _force = 0;
+                        player.Top = x.Bottom;
+                    } 
+                    if (player.Right > x.Left && player.Right < x.Right && player.Top < x.Bottom && player.Bottom > x.Top)
+                    {
+                        player.Left = x.Left - player.Width;
+                    } 
+                    if (player.Left < x.Right && player.Left > x.Left && player.Top < x.Bottom && player.Bottom > x.Top) 
+                    {
+                        player.Left = x.Right;
+                    }
+                }
+            }
+            Player_movements();
+        }
+
+        private void Player_movements()
+        {
             if (_right) player.Left += 5;
             if (_left) player.Left -= 5;
             if (_jump)
@@ -42,7 +72,12 @@ namespace Platform
                 player.Top -= _force;
                 _force -= 1;
             }
-            if (player.Top + player.Height >= this.ClientSize.Height) _jump = false;
+
+            if (player.Top + player.Height >= this.ClientSize.Height)
+            {
+                player.Top = this.ClientSize.Height - player.Height;
+                _jump = false;
+            }
             else player.Top += 5;
         }
     }
