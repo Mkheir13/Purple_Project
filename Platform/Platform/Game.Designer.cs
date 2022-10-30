aa2d0fe21116a6f0d41607be6400d9f71ff8824b
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-10-30 23:23:40 +0100

    jump fix

--- Platform/Platform/Game.Designer.cs
+++ Platform/Platform/Game.Designer.cs
@@ -31,18 +31,21 @@
         {
             this.player = new System.Windows.Forms.PictureBox();
             this.timer = new System.Timers.Timer();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.timer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // player
             // 
-            this.player.BackColor = System.Drawing.Color.Coral;
-            this.player.Location = new System.Drawing.Point(12, 400);
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Platform.Properties.Resources.finnidle;
+            this.player.Location = new System.Drawing.Point(12, 348);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(30, 50);
+            this.player.Size = new System.Drawing.Size(62, 62);
             this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.player.TabIndex = 0;
             this.player.TabStop = false;
@@ -55,12 +58,21 @@
             this.timer.SynchronizingObject = this;
             this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 416);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 13);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "platform";
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(302, 354);
+            this.pictureBox1.Location = new System.Drawing.Point(183, 377);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(322, 33);
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(110, 13);
+            this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Tag = "platform";
             // 
@@ -73,6 +85,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.player);
             this.DoubleBuffered = true;
             this.MaximizeBox = false;
@@ -83,9 +96,12 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.timer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.PictureBox pictureBox2;
 
         private System.Windows.Forms.PictureBox pictureBox1;
 
--- Platform/Platform/Game.cs
+++ Platform/Platform/Game.cs
@@ -6,8 +6,8 @@ namespace Platform
 {
     public partial class Form1 : Form
     {
-        bool _right, _left, _jump;
-        int _gravity = 15;
+        bool _right, _left, _jump, _isonfloor;
+        int _gravity = 10;
         int _force;
         public Form1()
         {
@@ -22,7 +22,7 @@ namespace Platform
             {
                 if (e.KeyCode == Keys.Space) {
                     _jump = true; 
-                    _force = _gravity; 
+                    _force = _gravity;
                 } 
             }
         }
@@ -35,50 +35,53 @@ namespace Platform
 
         private void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            foreach (Control x in this.Controls)
+            Collisions();
+            Player_movements();
+        }
+
+        private void Collisions()
+        {
+            if (_jump)
+            {
+                player.Top -= _force;
+                _force -= 1;
+            }
+            else player.Top += 5; 
+            foreach (Control x in this.Controls) 
             { 
-                if (x is PictureBox && (string)x.Tag == "platform")
-                { 
+                if (x is PictureBox && (string)x.Tag == "platform") 
+                {
                     if (player.Bottom > x.Top && player.Bottom < x.Bottom && player.Left < x.Right && player.Right > x.Left)
                     {
-                        _jump = false;
-                        _force = _gravity;
                         player.Top = x.Top - player.Height;
-                    } 
+                        _isonfloor = true;
+                        _jump = false;
+                    }
+
                     if (player.Top < x.Bottom && player.Top > x.Top && player.Left < x.Right && player.Right > x.Left)
                     {
                         _force = 0;
                         player.Top = x.Bottom;
-                    } 
+                    }
+
                     if (player.Right > x.Left && player.Right < x.Right && player.Top < x.Bottom && player.Bottom > x.Top)
                     {
                         player.Left = x.Left - player.Width;
-                    } 
-                    if (player.Left < x.Right && player.Left > x.Left && player.Top < x.Bottom && player.Bottom > x.Top) 
+                    }
+
+                    if (player.Left < x.Right && player.Left > x.Left && player.Top < x.Bottom && player.Bottom > x.Top)
                     {
                         player.Left = x.Right;
                     }
                 }
             }
-            Player_movements();
         }
 
         private void Player_movements()
         {
             if (_right) player.Left += 5;
             if (_left) player.Left -= 5;
-            if (_jump)
-            {
-                player.Top -= _force;
-                _force -= 1;
-            }
-
-            if (player.Top + player.Height >= this.ClientSize.Height)
-            {
-                player.Top = this.ClientSize.Height - player.Height;
-                _jump = false;
-            }
-            else player.Top += 5;
+            ;
         }
     }
 }
