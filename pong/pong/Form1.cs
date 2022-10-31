ed6225f1f8ddd85c6fe083dd8e51972dde400708
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-10-31 16:12:33 +0100

    Cpu play

--- pong/pong/Form1.cs
+++ pong/pong/Form1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Timers;
+﻿using System.Timers;
 using System.Windows.Forms;
 
 namespace pong
@@ -10,11 +9,12 @@ namespace pong
         {
             InitializeComponent();
         }
-        int _ballspeed = 5;
         bool _goUp;
         bool _goDown;
-        int _ballX = 5;
-        int _ballY = 5;
+        bool _goZ;
+        bool _goS;
+        int _ballX = 10;
+        int _ballY = 10;
         int _score;
         int _playertwoScore;
 
@@ -22,27 +22,40 @@ namespace pong
         {
             if (e.KeyCode == Keys.Up) _goUp = true;
             if (e.KeyCode == Keys.Down) _goDown = true;
+            if (e.KeyCode == Keys.Z) _goZ = true;
+            if (e.KeyCode == Keys.S) _goS = true;
         }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Up) _goUp = false;
             if (e.KeyCode == Keys.Down) _goDown = false;
+            if (e.KeyCode == Keys.Z) _goZ = false;
+            if (e.KeyCode == Keys.S) _goS = false;
         }
 
         private void Timer(object sender, ElapsedEventArgs e)
         {
+            //increase ball speed with the score
+
             PlayerScore.Text = "" + _score;
             CpuScore.Text = "" + _playertwoScore;
             Player_movement();
+            Cpumovement();
             Ball_movement();
             Player_Collision();
+            Cpu_Collision();
             Win();
         }
 
         private void Player_movement()
         {
-            if (_goUp && playerone.Top > this.panel1.Top) playerone.Top -= 8;
-            if (_goDown && playerone.Bottom < this.panel1.Bottom - playerone.Width) playerone.Top += 8;
+            if (_goUp && playertwo.Top > this.panel1.Top) playertwo.Top -= 8;
+            if (_goDown && playertwo.Bottom < this.panel1.Bottom - playertwo.Width) playertwo.Top += 8;
+        }
+        private void Cpumovement()
+        {
+            if (_goZ && playerone.Top > this.panel1.Top) playerone.Top -= 8;
+            if ( _goS && playerone.Bottom < this.panel1.Bottom - playerone.Width) playerone.Top += 8;
         }
         private void Ball_movement()
         {
@@ -83,18 +96,37 @@ namespace pong
                 }
             }
         }
+        private void Cpu_Collision()
+        {
+            if (ball.Bounds.IntersectsWith(playertwo.Bounds))
+            {
+                if (ball.Right < playertwo.Left) _ballY = -_ballY;
+                else _ballX = -_ballX;
+                if (ball.Bottom > playertwo.Top && ball.Top < playertwo.Bottom) _ballY = -_ballY;
+                else _ballX = -_ballX;
+                if (ball.Right > playertwo.Right + 40)
+                {
+                    ball.Left = panel1.Width / 2;
+                    ball.Top = panel1.Height / 2;
+                    _ballX = -_ballX;
+                    _ballY = -_ballY;
+                }
+            }
+        }
         private void Win()
         {
-            if (_score == 2)
+            if (_score == 10)
             {
-                MessageBox.Show("Player One Wins!");
+                timer1.Stop();
+                MessageBox.Show(@"Player One Wins!");
                 _score = 0;
                 _playertwoScore = 0;
-                
+
             }
             if (_playertwoScore == 10)
             {
-                MessageBox.Show("Player Two Wins!");
+                timer1.Stop();
+                MessageBox.Show(@"Player Two Wins!");
                 _score = 0;
                 _playertwoScore = 0;
             }
